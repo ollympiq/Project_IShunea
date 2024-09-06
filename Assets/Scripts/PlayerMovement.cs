63cd84af3e94f8ac4fd097db83590a51921cde8e
ollympiq <158150699+ollympiq@users.noreply.github.com>
2024-09-06 20:04:31 +0300

    Player Movement/Animation update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,42 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float playerSpeed = 10f;
     [SerializeField] private float playerJumpSpeed = 6f;
     private Rigidbody2D playerBody;
-    
-
+    private Animator animator;
+    public bool grounded;
     private void Awake()
     {
         playerBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        playerBody.velocity = new Vector2(Input.GetAxis("Horizontal")*playerSpeed,playerBody.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        playerBody.velocity = new Vector2(horizontalInput * playerSpeed,playerBody.velocity.y);
 
-        if (Input.GetKey(KeyCode.Space)) 
-        playerBody.velocity = new Vector2 (playerBody.velocity.x, playerJumpSpeed);
+        //flip
+        if(horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3 (-1,1,1);
+
+        if (Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+        //set animator parameter
+
+        animator.SetBool("run",horizontalInput != 0 );
+        animator.SetBool("grounded", grounded);
+    }
+    private void Jump() 
+    {
+        playerBody.velocity = new Vector2(playerBody.velocity.x, playerJumpSpeed);
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground") 
+        grounded = true;
     }
 }
